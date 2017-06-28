756cfe03d1787c30f6b6bbc55e9e3bc12a01c137
Andy Knolla <andyknolla@gmail.com>
2017-06-28 10:40:38 -0600

    Created a comment broadcast, configured the websocket route, and updated the comment model w/ a callback

--- config/routes.rb
+++ config/routes.rb
@@ -1,40 +1,23 @@
 Rails.application.routes.draw do
-
   resources :comments
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
-  # customize route for an individual portfolio
+
   resources :portfolios, except: [:show] do
     put :sort, on: :collection
   end
 
-
-
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+  get 'about-me', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+  get 'tech-news', to: 'pages#tech_news'
 
-  # for practice only
-  get 'angular-items', to: 'portfolios#angular_items'
-  get 'ruby-items', to: 'portfolios#ruby'
-  ###################
   resources :blogs do
     member do
       get :toggle_status
     end
   end
 
-  # default...
-  # get 'pages/home'
-  # get 'pages/about'
-  # get 'pages/contact'
-
-  # customized...controller#action, as in 'rake routes' listing.
+  mount ActionCable.server => '/cable'
+  
   root to: 'pages#home'
-
-  get 'about-me', to: 'pages#about'
-  get 'contact', to: 'pages#contact'
-  get 'tech-news', to: 'pages#tech_news'
-
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
-
-# AK: when 'pages/home' route is hit, the home action in the pages controller
