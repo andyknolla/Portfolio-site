3832ba9ae795c0bf6ef204f14309cb893613ef5f
Andy Knolla <andyknolla@gmail.com>
2017-07-02 01:43:03 -0600

    Style mods for navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -38,10 +38,10 @@ module ApplicationHelper
         url: portfolios_path,
         title: "Portfolio"
       },
-      {
-        url: tech_news_path,
-        title: "Tech News"
-      },
+      # {
+      #   url: tech_news_path,
+      #   title: "Tech News"
+      # },
     ]
   end
 
