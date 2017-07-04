1b80c1ad310a739c5cf4cf45c0631705f94d5e4d
Andy Knolla <andyknolla@gmail.com>
2017-07-04 11:51:39 -0600

    Content icons and styling, changed site title

--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -6,7 +6,7 @@ module DefaultPageContent
   end
 
   def set_page_defaults
-    @page_title = "Devcamp Portfolio | My Portofolio Website"
+    @page_title = "AndyKnolla"
     @seo_keywords = "Andys Portfolio, etc"
   end
 end
