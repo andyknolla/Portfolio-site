b93356f7202698fd6806dd6b931261c9de132bf7
Andy Knolla <andyknolla@gmail.com>
2017-07-04 15:39:08 -0600

    Style fixes and mods

--- app/assets/javascripts/landing.js
+++ app/assets/javascripts/landing.js
@@ -1,11 +1,7 @@
 $(document).ready(function() {
-
-
   $('.landing-scroll').on('click',function() {
-    console.log('scroll');
     $('html, body').animate({
       scrollTop: $("#landing-profile").offset().top
     }, 1000);
-
   });
 })
--- app/assets/javascripts/video_controls.js
+++ app/assets/javascripts/video_controls.js
@@ -1,24 +1,21 @@
 $(document).ready(function() {
 
-// add pause persistence b/w pages
+  $('#pauseVid').show();
+  $('#pauseVid').css("visibility",'visible');
 
-// add session storage state change when user clicks pause_vid
-// check session storage variable each time a new page loads
+  $('#pauseVid').on('click', function() {
+    console.log('pause');
+    var vid = document.getElementById('background');
+    vid.pause();
+    $('#pauseVid').hide();
+    console.log($('#pauseVid') == true);
+    $('#playVid').show();
+  })
 
-
-  $(window).on('mousemove',function() {
-    $('#pauseVid').on('click', function() {
-      var vid = document.getElementById('background');
-      vid.pause();
-      $('#pauseVid').hide();
-      $('#playVid').show();
-    })
-
-    $('#playVid').on('click', function() {
-      var vid = document.getElementById('background');
-      vid.play();
-      $('#playVid').hide();
-      $('#pauseVid').show();
-    })
-  });
+  $('#playVid').on('click', function() {
+    var vid = document.getElementById('background');
+    vid.play();
+    $('#playVid').hide();
+    $('#pauseVid').show();
+  })
 })
