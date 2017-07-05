61bbf73cadc260bdc2a49e441bf03d72d33641e3
Andy Knolla <andyknolla@gmail.com>
2017-07-04 18:41:01 -0600

    Tweaked vertical layout elements and vid-controls variable display/visibility

--- app/assets/javascripts/video_controls.js
+++ app/assets/javascripts/video_controls.js
@@ -1,21 +1,25 @@
 $(document).ready(function() {
 
-  $('#pauseVid').show();
-  $('#pauseVid').css("visibility",'visible');
+  if ( $(window).width() > 736 ) {
+    $('#pauseVid').css("visibility",'visible');
+  };
 
   $('#pauseVid').on('click', function() {
     console.log('pause');
     var vid = document.getElementById('background');
     vid.pause();
     $('#pauseVid').hide();
-    console.log($('#pauseVid') == true);
+    $('#pauseVid').css("visibility",'hidden');
     $('#playVid').show();
+    $('#playVid').css("visibility",'visible');
   })
 
   $('#playVid').on('click', function() {
     var vid = document.getElementById('background');
     vid.play();
     $('#playVid').hide();
+    $('#playVid').css("visibility",'hidden');
     $('#pauseVid').show();
+    $('#pauseVid').css("visibility",'visible');
   })
 })
