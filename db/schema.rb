550576fb6a065a180cbd8bf1e9611b01fa5a1004
Andy Knolla <andyknolla@gmail.com>
2017-07-06 16:36:19 -0600

    Added github link column to profile table, and related elements in views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170705172850) do
+ActiveRecord::Schema.define(version: 20170706221257) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20170705172850) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "position"
+    t.string "github_link"
   end
 
   create_table "skills", id: :serial, force: :cascade do |t|
