ef3e512cb768eebefbf3f512517539090354984f
Andy Knolla <andyknolla@gmail.com>
2017-07-06 21:08:02 -0600

    Added resume to about and navbar, fixed navbar width

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,4 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( blogs.css )
 Rails.application.config.assets.precompile += %w( portfolios.css )
 Rails.application.config.assets.precompile += %w( landing.css )
+Rails.application.config.assets.precompile += %w( pages.css )
