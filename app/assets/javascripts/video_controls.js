8c140a13119cc9cc84bc23a28e299c4480aada3e
Andy Knolla <andyknolla@gmail.com>
2017-08-11 12:03:42 -0600

    Changed bg default to image, made video optional

--- app/assets/javascripts/video_controls.js
+++ app/assets/javascripts/video_controls.js
@@ -1,25 +1,30 @@
 $(document).ready(function() {
-
   if ( $(window).width() > 736 ) {
-    $('#pauseVid').css("visibility",'visible');
+    $('#playVid').css('visibility','visible');
   };
-
-  $('#pauseVid').on('click', function() {
-    console.log('pause');
-    var vid = document.getElementById('background');
-    vid.pause();
-    $('#pauseVid').hide();
-    $('#pauseVid').css("visibility",'hidden');
-    $('#playVid').show();
-    $('#playVid').css("visibility",'visible');
-  })
-
+  var firstPress = true;
   $('#playVid').on('click', function() {
-    var vid = document.getElementById('background');
+
+    if(firstPress === true) {
+      $('#background-img').hide();
+      $('#background-vid').show();
+    }
+    firstPress = false;
+
+    var vid = document.getElementById('background-vid');
     vid.play();
     $('#playVid').hide();
-    $('#playVid').css("visibility",'hidden');
+    $('#playVid').css('visibility','hidden');
     $('#pauseVid').show();
-    $('#pauseVid').css("visibility",'visible');
+    $('#pauseVid').css('visibility','visible');
+  })
+
+  $('#pauseVid').on('click', function() {
+    var vid = document.getElementById('background-vid');
+    vid.pause();
+    $('#pauseVid').hide();
+    $('#pauseVid').css('visibility','hidden');
+    $('#playVid').show();
+    $('#playVid').css('visibility','visible');
   })
 })
