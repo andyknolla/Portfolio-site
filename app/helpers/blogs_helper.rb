ac6d9f16eee2e3458f1564538900273c5ba74a4e
Andy Knolla <andyknolla@gmail.com>
2017-08-16 17:25:27 -0600

    Fixed userbar height, tweaked markdown

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -12,7 +12,8 @@ module BlogsHelper
       fenced_code_blocks: true,
       no_intra_emphasis: true,
       autolink: true,
-      lax_html_blocks: true
+      lax_html_blocks: true,
+      underline: true
     }
     markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
     markdown_to_html.render(text).html_safe
